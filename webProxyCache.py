c714d71bc0df9b2cb4ea36460e41e714ba22ea7b
robertolozano <rlozano@ucdavis.edu>
2021-03-09 15:09:46 -0800

    Update webProxyCache.py

--- webProxyCache.py
+++ webProxyCache.py
@@ -15,6 +15,7 @@ tcpSerSock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 tcpSerSock.bind((sys.argv[1], 8888))
 tcpSerSock.listen(100)
 # Fill in end.
+refererFlag = 0
 
 while 1:
     # Start receiving data from the client
@@ -23,11 +24,20 @@ while 1:
     tcpCliSock, addr = tcpSerSock.accept()
     print('Received a connection from:', addr)
     message = tcpCliSock.recv(1024)  # Fill in start. # Fill in end.
+    originalMessage = message
     print(message)
 
-    # Extract the filename from the given message
-    # print(message.split()[1])
-    filename = message.split()[1].partition(b"/")[2]
+    try:
+        #Extract Search Term
+        search = message.split()[1]
+        print(search)
+
+        #Remove first parantheses from search term
+        filename = message.split()[1].partition(b"/")[2]
+        hostn = filename.partition(b"/")[0]
+        print("hostname:",hostn)
+    except:
+        continue
     filename = filename.decode('utf-8')
 
     print("filename:", filename)
@@ -35,9 +45,24 @@ while 1:
     filetouse = "/" + filename
     print("filetouse:", filetouse)
 
+    if b"Referer" in message:
+        refererFlag = 1
+        print("REFERER EXISTS")
+        print(message.partition(b"Referer: http://localhost:8888")[2])
+        message = message.partition(b"Referer: http://localhost:8888")[2]
+        print(message.partition(b"/")[2])
+        message = message.partition(b"/")[2]
+        # filename = message.partition(b"/")[0]
+        filename = message.decode()
+        print(filename.partition('\r')[0])
+        filename = filename.partition('\r')[0]
+        hostn = filename.partition('/')[0]
+        print("hostname:", hostn)
+
     try:
         # Check whether the file exist in the cache
         f = open(filetouse[1:], "r")
+        print("File exists")
         outputdata = f.readlines()
         fileExist = "true"
         # ProxyServer finds a cache hit and generates a response message
@@ -51,36 +76,37 @@ while 1:
 
     # Error handling for file not found in cache
     except IOError:
+        print("File doesn't exist")
         if fileExist == "false":
             # Create a socket on the proxyserver
             c = socket(AF_INET, SOCK_STREAM)  # Fill in start. # Fill in end.
-            hostn = filename.replace("www.", "", 1)
+            # hostn = filename#.replace("www.", "", 1)
             print("hostn:", hostn)
 
             try:
                 # Connect to the socket to port 80
-                # Fill in start.
+                # hostn = "www.cs.toronto.edu"
+
+                print("connecting to host", hostn)
                 c.connect((hostn, 80))
-                # Fill in end.
                 # Create a temporary file on this socket and ask port 80 for the file requested by the client
 
-                request = "GET "+"http://" + hostn + " HTTP/1.0\r\n\r\n"
+                # request = "GET "+"http://" + hostn + " HTTP/1.0\r\n\r\n"
+                request = "GET "+"http://" + filename + " HTTP/1.0\r\n\r\n"
+                print("request:", request)
 
-                if "css" in filename:
-                    cssPath = filename
-                    print("found css")
-                    filename = message.partition(b"Referer:")[2]
 
-                    print(filename)
-                    filename = message.partition(b"Referer:")[2].partition(b"www.")
-                    print("1:", filename)
-                    filename = filename[1] + filename[2]
-                    print("2:", filename)
-                    filename = filename.partition(b"\r")[0]
-                    print("PATH:", filename + filetouse.encode())
-                    filename = filename + filetouse.encode()
-                    filename = filename.decode("utf-8")
-                    request = "GET "+"http://" + hostn + " HTTP/1.0\r\n\r\n"
+                if refererFlag == 1:
+                    print("Search term:",originalMessage.split()[1])
+                    search = originalMessage.split()[1]
+                    search = search.decode()
+                    if search[0] == "/":
+                        request = "GET http://" + search + " HTTP/1.0\r\n\r\n"
+                    else:
+                        request = "GET http://" + search + " HTTP/1.0\r\n\r\n"
+                    print(request)
+                    refererFlag = 0
+
 
                 print("request:", request)
                 c.send(request.encode())
@@ -96,31 +122,17 @@ while 1:
 
                 tcpCliSock.send(totalMessage)
 
-                print()
-                tempMessage = tcpCliSock.recv(1024)
-                print(tempMessage)
-
-                #Read the response into buffer
-
-                #Fill in start.
-                #Fill in end.
-
                 #Create a new file in the cache for the requested file.
                 #Also send the response in the buffer to client socket and the corresponding file in the cache
-                tmpFile = open("./" + filename, "wb")
-                
-                #Fill in start.
-                tmpFile.write(totalMessage)
-                # Fill in end.
+                # tmpFile = open("./" + filename, "wb")
+                # tmpFile.write(totalMessage)
             except:
                 print("Illegal request")
         else:
             # HTTP response message for file not found
-            # Fill in start.
             tcpCliSock.send("HTTP/1.0 404 sendErrorErrorError\r\n".encode('utf-8'))
             tcpCliSock.send("Content-Type:text/html\r\n".encode('utf-8'))
             tcpCliSock.send("\r\n".encode('utf-8'))
-            # Fill in end.
 # Close the client and the server sockets
 tcpCliSock.close()
 
